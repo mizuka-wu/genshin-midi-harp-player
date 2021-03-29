d2d042e960635557272973c13e3ea51731ab53ab
水华 <mizuka.wu@outlook.com>
2021-03-29 22:58:06 +0800

    feat: 初始化

---
+++ index.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+const MidiPlayer = require('midi-player-js')
+const throttle = require('lodash/throttle')
+const robot = require('robotjs')
+const fs = require('fs')
+const path = require('path')
+const countdown = require('node-countdown')
+
+const KEYBOARD_NOTENUMBER = JSON.parse(fs.readFileSync(
+  path.join(__dirname, 'keyboard.json')
+  , 'utf-8'))
+
+/**
+ * 按键
+ */
+const pressKey = throttle(function (key) {
+  robot.keyTap(key)
+}, 50)
+
+const Player = new MidiPlayer.Player(function ({ noteNumber, noteName }) {
+  const pianoNoteNumber = noteNumber - 20
+  const key = KEYBOARD_NOTENUMBER[pianoNoteNumber + '']
+
+  if (isNaN(pianoNoteNumber)) {
+    console.log('空音节')
+  }
+
+  if (key) {
+    pressKey(key)
+    console.log('按下：', noteName, pianoNoteNumber, key)
+  } else {
+    console.log('未找到对应按键：', noteName, pianoNoteNumber)
+  }
+})
+
+const filePath = path.join(__dirname, 'data.mid')
+
+if (fs.existsSync(filePath)) {
+// Load a MIDI file
+  Player.loadFile(filePath)
+  countdown.start(5000, { suffix: '后开始备份' }, function (err) {
+    err
+      ? console.log(err.message)
+      : console.log('开始备份...')
+  })
+// setTimeout(() => {
+//     Player.play()
+// }, 3000)
+} else {
+  console.log('请在当前目录放置data.mid')
+}
