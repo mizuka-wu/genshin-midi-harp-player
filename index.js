2ddfc30746231fd97d136ae2ce7c1e901545c0c8
水华 <mizuka.wu@outlook.com>
2021-03-30 21:24:39 +0800

    feat: 替换对应关系表

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const MidiPlayer = require('midi-player-js')
-const throttle = require('lodash/throttle')
+// const throttle = require('lodash/throttle')
 const robot = require('robotjs')
 const fs = require('fs')
 const path = require('path')
@@ -13,23 +13,22 @@ const KEYBOARD_NOTENUMBER = JSON.parse(fs.readFileSync(
 /**
  * 按键/支持按多个
  */
-const pressKey = throttle(function (keys) {
+const pressKey = function (keys) {
   keys.split('').forEach(key => robot.keyTap(key))
-}, 50)
+}
 
-const Player = new MidiPlayer.Player(function ({ noteNumber, noteName }) {
-  const pianoNoteNumber = noteNumber - 20
-  const key = KEYBOARD_NOTENUMBER[pianoNoteNumber + '']
+const Player = new MidiPlayer.Player(function ({ noteName }) {
+  const key = KEYBOARD_NOTENUMBER[noteName]
 
-  if (isNaN(pianoNoteNumber)) {
+  if (!noteName) {
     console.log('空音节')
   }
 
   if (key) {
     pressKey(key)
-    console.log('按下：', noteName, pianoNoteNumber, key)
+    console.log('按下：', noteName, key)
   } else {
-    console.log('未找到对应按键：', noteName, pianoNoteNumber)
+    console.log('未找到对应按键：', noteName)
   }
 })
 
